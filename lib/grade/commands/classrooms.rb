dd3f9e21b481d8c2fc2d8e4789daa14d3a56ca84
Matheus Ferreira <matheusmlfg@gmail.com>
2026-04-07 12:49:07 -0300

    Refacts Commands::Classroom to use methods and respect D.R.Y.

--- lib/grade/commands/classrooms.rb
+++ lib/grade/commands/classrooms.rb
@@ -5,48 +5,14 @@ module Grade
 
       desc "list", "List all Classrooms"
       def list
-        pastel = Pastel.new
-        
         loading_search
-        result = Services::Classrooms::List.call(client: client)
-
-        if result[:ok]
-          @spinner.success(pastel.green("Done!"))
-
-          table = TTY::Table.new(header: ["Class code", "Class name", "Room", "Time slot"])
-          result[:data].each do |item| 
-            table << [item["cod"], item["name"], item["room"], item["time_slot"].upcase]
-          end
-
-          say table.render(:unicode, alignments: [:center, :left, :center, :center])
-        else
-          @spinner.error("Error!")
-          say result[:error], :red
-          exit 1
-        end
+        handle_result(result: Services::Classrooms::List.call(client: client))
       end
 
       desc "now", "Show the actual class info"
       def now
-        pastel = Pastel.new
-
         loading_search
-        result = Services::Classrooms::Now.call(client: client)
-
-        if result[:ok]
-          @spinner.success(pastel.green("Done!"))
-          
-          table = TTY::Table.new(header: ["Class code", "Class name", "Room", "Time slot"])
-          result[:data].each do |item|
-            table << [item["cod"], item["name"], item["room"].upcase, item["time_slot"].upcase]
-          end
-
-           say table.render(:unicode, alignments: [:center, :left, :center, :center])
-        else
-          @spinner.error("Error!")
-          say result[:error], :red
-          exit 1
-        end
+        handle_result(result: Services::Classrooms::Now.call(client: client))
       end
 
       private
@@ -55,6 +21,29 @@ module Grade
         @spinner = TTY::Spinner.new("[:spinner] Searching...", format: :dots)
         @spinner.auto_spin
         sleep(1)
+      end
+
+      def format_table(result:)
+        table = TTY::Table.new(header: ["Class code", "Class name", "Room", "Time slot"])
+          result[:data].each do |item| 
+            table << [item["cod"], item["name"], item["room"], item["time_slot"].upcase]
+          end
+        table
+      end
+
+      def pastel
+        @pastel ||= Pastel.new
+      end
+
+      def handle_result(result:)
+        if result[:ok]
+          @spinner.success(pastel.green("Done!"))
+          say format_table(result: result).render(:unicode, alignments: [:center, :left, :center, :center])
+        else
+          @spinner.error("Error!")
+          say result[:error], :red
+          exit 1
+        end
       end
     end 
   end
